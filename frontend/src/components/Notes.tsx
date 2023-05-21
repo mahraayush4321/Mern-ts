f5fb1a5585961cdadb3f5be1a482ab704b64420f
mahraayush4321 <tanishqmahra@gmail.com>
2023-05-21 07:39:34 +0530

    FRONTTS

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Note as NoteModel } from "../models/INotes";
 import { Card } from "react-bootstrap";
 import styles from "../styles/Note.module.css";
+import { formatDate } from "../utils/formatDate";
 
 interface NotesProps {
   note: NoteModel;
@@ -11,12 +12,20 @@ interface NotesProps {
 const Notes = ({ note, className }: NotesProps) => {
   const { title, text, createdAt, updatedAt } = note;
 
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  }
+
   return (
     <Card className={`${styles.noteCard} ${className}`}>
-      <Card.Body>
+      <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
+      <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
     </Card>
   );
 };
