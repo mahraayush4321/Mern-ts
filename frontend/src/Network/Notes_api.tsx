291d38d8de548faadf17ceb841a78b04a1f6a133
mahraayush4321 <tanishqmahra@gmail.com>
2023-05-26 08:30:34 +0530

    UPDATED

--- frontend/src/Network/Notes_api.tsx
+++ frontend/src/Network/Notes_api.tsx
@@ -12,9 +12,12 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
 }
 
 export async function fetchNotes(): Promise<Note[]> {
-  const response = await fetchData("/api/notes", {
-    method: "GET",
-  });
+  const response = await fetchData(
+    `https://notes-ujpw.onrender.com/api/notes`,
+    {
+      method: "GET",
+    }
+  );
   return response.json();
 }
 
@@ -24,13 +27,16 @@ export interface noteInput {
 }
 
 export async function createNote(note: noteInput): Promise<Note> {
-  const response = await fetchData("/api/notes", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(note),
-  });
+  const response = await fetchData(
+    `https://notes-ujpw.onrender.com/api/notes`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    }
+  );
   return response.json();
 }
 
@@ -38,18 +44,21 @@ export async function updateNote(
   noteId: string,
   note: noteInput
 ): Promise<Note> {
-  const response = await fetchData("/api/notes/" + noteId, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(note),
-  });
+  const response = await fetchData(
+    `https://notes-ujpw.onrender.com/api/notes/` + noteId,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    }
+  );
   return response.json();
 }
 
 export async function deleteNote(noteId: string) {
-  await fetchData("/api/notes/" + noteId, {
+  await fetchData(`https://notes-ujpw.onrender.com/api/notes/` + noteId, {
     method: "DELETE",
   });
 }
